9e887777bd039d15b57bd9a9b744e22913c37367
thelouisgram <hadesanoye01@gmail.com>
2023-09-15 06:34:03 +0100

    more

--- src/components/Shared/Dots.jsx
+++ src/components/Shared/Dots.jsx
@@ -2,7 +2,7 @@
 const Dots = ({ count, selectedMovieIndex, setSelectedMovieIndex }) => {
     const dots = Array.from({ length: count }, (_, index) => (
         <button onClick={()=>setSelectedMovieIndex(index)}
-        className={` ${selectedMovieIndex === index ? 'text-[40px] text-white' : 'text-[32px] text-gray-400 hover:text-white' }`} key={index}>•</button>
+        className={` ${selectedMovieIndex === index ? 'text-[40px] text-white' : 'text-[32px] text-gray-500 hover:text-white' }`} key={index}>•</button>
     ));
 
     return <div className="flex gap-[6px] items-center">{dots}</div>;
